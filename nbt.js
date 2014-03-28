f1d7f5c1096de0269bacf4b810250ea5f91cce87
deathcap <deathcap@gmx.co.uk>
2014-03-27 22:20:01 -0700

    Fix 64-bit upper half power (note may still lose precision, see GH-1)

--- nbt.js
+++ nbt.js
@@ -59,7 +59,7 @@
 			/* FIXME: this can overflow, JS has 53 bit precision */
 			var upper = this.int();
 			var lower = this.int();
-			return (upper << 32) + lower;
+			return (upper * 4294967296) + lower;
 		};
 
 		this[nbt.tagTypes.byteArray] = function() {
